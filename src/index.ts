f0f4ac93503cde75790cad991b8f7f3043c3ebe4
doinkythederp <mcclelll@icloud.com>
2022-02-04 16:56:27 -0800

    feat: export from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export * from './RESTClient';
+export { default } from './RESTClient';
+export * from './RESTError';
+export { default as RESTError } from './RESTError';
+export * from './util';
