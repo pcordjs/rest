e5dfc72a1107bfe256eb0072e88cfe04a6c4908d
doinkythederp <doinkythederp@icloud.com>
2022-03-21 13:35:16 -0700

    docs: document error classes

--- src/RESTError.ts
+++ src/RESTError.ts
@@ -1,13 +1,16 @@
 import { format } from 'node:util';
 
+/**
+ * Error codes for {@link RESTError} objects.
+ */
 export enum RESTErrorCode {
-  /** Used when a token is requried but cannot be found */
+  /** Used when a token is required but cannot be found */
   TOKEN_REQUIRED,
   /** Used when a request takes too long to finish and is cancelled. */
   TIMEOUT,
   /**
-   * Used when an invalid API version is provided.
-   * API versions must be a whole number greater than 0.
+   * Used when an invalid API version is provided. API versions must be a whole
+   * number greater than 0.
    */
   INVALID_API_VERSION
 }
@@ -19,6 +22,9 @@ const messages: Record<RESTErrorCode, string> = {
   [RESTErrorCode.INVALID_API_VERSION]: 'An invalid API version was provided.'
 };
 
+/**
+ * An error created by a {@link RESTClient}.
+ */
 export default class RESTError extends Error {
   public constructor(public readonly code: RESTErrorCode, ...args: unknown[]) {
     super(format(messages[code], ...args));
@@ -27,6 +33,13 @@ export default class RESTError extends Error {
   public override name = this.constructor.name;
 }
 
+/**
+ * An error created by the Discord API.
+ *
+ * @remarks
+ * If an API response did not fail with an error code, the
+ * {@link DiscordAPIError.code} property will be set to `-1`.
+ */
 export class DiscordAPIError extends Error {
   public constructor(
     public readonly code: number,
